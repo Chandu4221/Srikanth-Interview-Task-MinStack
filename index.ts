e7a57e9cdd52c422d722d8e4d8f82cf396c5bffe
Chandu4221 <chndraja@gmail.com>
2021-08-13 10:58:30 +0530

    added console logs

--- index.ts
+++ index.ts
@@ -9,14 +9,15 @@ appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 let minStack = new MinStack();
 
 minStack.push(-2);
-console.log(minStack.stack);
+console.log('After Push', minStack.stack);
 minStack.push(0);
-console.log(minStack.stack);
+console.log('After Push', minStack.stack);
 minStack.push(-3);
-console.log(minStack.stack);
+console.log('After Push', minStack.stack);
 let minValueInStack = minStack.getMin(); // return -3
 console.log('minValueInStack', minValueInStack);
 minStack.pop();
+console.log('After POP ', minStack.stack);
 const topItem = minStack.top(); // return 0
 console.log('topItem', topItem);
 minValueInStack = minStack.getMin(); // return -2
