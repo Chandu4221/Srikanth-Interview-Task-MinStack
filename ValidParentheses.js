6c63e93552cc7a55d364bf42c93d75f6c2e4b3f0
Chandu4221 <chndraja@gmail.com>
2021-08-13 17:07:29 +0530

    valid paranthesis added

--- ValidParentheses.js
+++ ValidParentheses.js
@@ -1,16 +1,21 @@
-function ValidParanthesis(input: String) {
+function ValidParanthesis(input) {
   const stack = [];
   const matchingBraces = {
     '{': '}',
     '(': ')',
     '[': ']'
   };
-
   const openingBraces = Object.keys(matchingBraces);
 
   for (let char in input) {
     if (openingBraces.includes(char)) {
-      
+      stack.push(char);
+    } else {
+      const lastItem = stack.pop();
+      if (char !== matchingBraces[lastItem]) return false;
     }
   }
+
+  if (stack.length !== 0) return false;
+  return true;
 }
